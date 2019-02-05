b3cadb9f199c7920fc48e44b46016614018fdc33
Paramtamtam <slashkot@gmail.com>
2019-02-05 13:44:24 +0500

    Fix env resolving order

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog][keepachangelog] and this project adheres to [Semantic Versioning][semver].
 
+## v1.0.2
+
+### Fixed
+
+- Methods `Worker->getDefaultAppBasePath()` and `Worker->getDefaultAppBootstrapPath()` (resolve order)
+
 ## v1.0.1
 
 ### Fixed
--- src/Worker/Worker.php
+++ src/Worker/Worker.php
@@ -182,7 +182,7 @@ class Worker implements WorkerInterface
      */
     protected function getDefaultAppBasePath(): string
     {
-        return env(static::ENV_NAME_APP_BASE_PATH, $_ENV[static::ENV_NAME_APP_BASE_PATH] ?? \dirname(__DIR__, 5));
+        return env(static::ENV_NAME_APP_BASE_PATH) ?? $_ENV[static::ENV_NAME_APP_BASE_PATH] ?? \dirname(__DIR__, 5);
     }
 
     /**
@@ -192,7 +192,7 @@ class Worker implements WorkerInterface
      */
     protected function getDefaultAppBootstrapPath(): string
     {
-        return env(static::ENV_NAME_APP_BOOTSTRAP_PATH, $_ENV[static::ENV_NAME_APP_BOOTSTRAP_PATH] ?? '/bootstrap/app.php');
+        return env(static::ENV_NAME_APP_BOOTSTRAP_PATH) ?? $_ENV[static::ENV_NAME_APP_BOOTSTRAP_PATH] ?? '/bootstrap/app.php';
     }
 
     /**
